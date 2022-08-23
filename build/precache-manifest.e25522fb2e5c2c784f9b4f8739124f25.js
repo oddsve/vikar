2fe54fa86efd45e19a0cf30095ae22813a64b7e9
oddsve <odd.morten@sveas.org>
2022-08-23 22:55:00 +0200

    lagt til et bygg

---
+++ build/precache-manifest.e25522fb2e5c2c784f9b4f8739124f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "518a81f80db27a712a90",
+    "url": "/static/css/main.cacf2b8a.chunk.css"
+  },
+  {
+    "revision": "518a81f80db27a712a90",
+    "url": "/static/js/main.518a81f8.chunk.js"
+  },
+  {
+    "revision": "c4cab35487dc8dcc0397",
+    "url": "/static/js/1.c4cab354.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3cb519bdda7640771b304eeb007cf6ff",
+    "url": "/index.html"
+  }
+];
